1dd40313de9f7466f8713d86f4a677a504ad28f2
Phil Ross <phil.ross@gmail.com>
2015-01-08 20:38:51 +0000

    Search for for libbz2.so.1 in addition to libbz2.so.
    Debian and Red Hat only install libbz2.so in the libbz2 development
    package.

--- lib/bzip2/ffi/libbz2.rb
+++ lib/bzip2/ffi/libbz2.rb
@@ -6,7 +6,7 @@ module Bzip2
     module Libbz2
       extend ::FFI::Library
 
-      ffi_lib ['bz2', 'libbz2.dll']
+      ffi_lib ['bz2', 'libbz2.so.1', 'libbz2.dll']
 
       BZ_RUN    = 0
       BZ_FLUSH  = 1
